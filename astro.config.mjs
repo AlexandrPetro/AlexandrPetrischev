00e22295ef870f995d0b7683f1c6500b9ee9e127
AlexandrPetro <105237990+AlexandrPetro@users.noreply.github.com>
2026-09-18 11:24:13 +0300

    chore: configure GitHub Pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://alexandrpetrischev.github.io',
+  site: 'https://alexandrpetro.github.io',
+  base: '/AlexandrPetrischev',
 });
