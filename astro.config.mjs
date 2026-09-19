10dade92a54520cfad5ca3831e8c4e74a521a36d
AlexandrPetro <105237990+AlexandrPetro@users.noreply.github.com>
2026-09-19 15:19:43 +0300

    Fix static project page URLs for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   site: 'https://alexandrpetro.github.io',
   base: '/AlexandrPetrischev',
+  trailingSlash: 'always',
 });
